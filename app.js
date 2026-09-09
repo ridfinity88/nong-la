6b09d6c410fd1981f3ef1326a4417bf64783ea43
cougar <ridfinity88@gmail.com>
2026-09-09 18:07:24 +0700

    Update app.js

--- app.js
+++ app.js
@@ -56,7 +56,6 @@
     $('registrationEmployeeId').addEventListener('input', () => clearFieldError('registrationEmployeeId'));
     $('newEmployeeForm').addEventListener('submit', createNewEmployee);
     $('newEmployeeFullName').addEventListener('input', () => clearFieldError('newEmployeeFullName'));
-    $('newEmployeeBackBtn').addEventListener('click', backToEmployeeRegistration);
     $('leaveForm').addEventListener('submit', onReviewSubmit);
     $('reason').addEventListener('input', (e) => $('reasonCount').textContent = e.target.value.length);
     $$('input[name="leaveUnit"]').forEach(el => el.addEventListener('change', updateLeaveUnitUi));
