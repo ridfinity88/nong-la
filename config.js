739eb3e68181241d3f09556da2cacd5d1cc1f848
cougar <ridfinity88@gmail.com>
2026-09-09 17:56:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ window.APP_CONFIG = Object.freeze({
   LIFF_ID: '2011499508-tPfya4Ma',
   GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbyreQ5tQdGvIkQKebr5rhu9XNWwYOodG67d-1J0IbRSPzW6cEzY1y4TIeKSP_Zj3Qn4JQ/exec',
   DEPARTMENT: 'วิศวกรรมผลิต 1',
-  APP_NAME: 'Leave Management',
+  APP_NAME: 'KALALAND',
   MOBILE_ONLY: true,
   REQUIRE_GROUP_CONTEXT: true,
   DEFAULT_DASHBOARD_MODE: 'DAILY',
