7a248216cc4c52eecf78e2ac0be6ef502ee0bf91
cougar <ridfinity88@gmail.com>
2026-09-09 18:29:21 +0700

    Update APP_NAME in config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ window.APP_CONFIG = Object.freeze({
   LIFF_ID: '2011499508-tPfya4Ma',
   GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbyreQ5tQdGvIkQKebr5rhu9XNWwYOodG67d-1J0IbRSPzW6cEzY1y4TIeKSP_Zj3Qn4JQ/exec',
   DEPARTMENT: 'วิศวกรรมผลิต 1',
-  APP_NAME: 'KALALAND',
+  APP_NAME: 'สวัสดีหนุ่มกะลา',
   MOBILE_ONLY: true,
   REQUIRE_GROUP_CONTEXT: true,
   DEFAULT_DASHBOARD_MODE: 'DAILY',
