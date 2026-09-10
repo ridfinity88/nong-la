8df4f9cba9a3e7be837845f1707497457a8d27d8
cougar <ridfinity88@gmail.com>
2026-09-10 16:56:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = Object.freeze({
   LIFF_ID: '2011499508-tPfya4Ma',
-  GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbyVbnOUTXfdHmKb3G9id54yDJgZZ2bbKXi6eqpfpKqj27C7SsIwb9nRGyPHOw5M1xaK1Q/exec',
+  GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbx6W-YOYZtuPqDOjL8y3IQ28-x_RhN3L_y3PuJ9QJkOG3SzhWqDAo61WABkfJPqgkYI3A/exec',
   DEPARTMENT: 'วิศวกรรมผลิต 1',
   APP_NAME: 'สวัสดีหนุ่มกะ(จะ)ลา',
   MOBILE_ONLY: true,
