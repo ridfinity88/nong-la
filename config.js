3149d536f0f29054e9d3746092bd0062a25cbfaa
cougar <ridfinity88@gmail.com>
2026-09-10 18:39:42 +0700

    Update GAS_WEB_APP_URL in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = Object.freeze({
   LIFF_ID: '2011499508-tPfya4Ma',
-  GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbwXsDNmIkUwWDvB-f1R_nlmyI525CoCiBIcz_w7KBA5iR6NFColV5UIVrgypUEt0o-Jfw/exec',
+  GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbwXGjDsohR3jKOuzyUetu0Bhew44hffN0_GS45xjssrslXpr7lGZvysRJROq6xgscivFA/exec',
   DEPARTMENT: 'วิศวกรรมผลิต 1',
   APP_NAME: 'Leave Management',
   MOBILE_ONLY: true,
