c930161a3c96b1267c6aa6fd3d466def1de1d574
cougar <ridfinity88@gmail.com>
2026-09-10 18:33:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = Object.freeze({
   LIFF_ID: '2011499508-tPfya4Ma',
-  GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxwzhsmqq5_TSaq8N_jAc7LUsObRVjtEXgZX_dvkBLFrjy6yTQspnQwWl-5It19SMsuXA/exec',
+  GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbyfvFcu8xxHEXWdhNxEuljicWAt2Cv43lHt5tjQPFSMdAFM2TNyFgej_XrFhk_bgTs47Q/exec',
   DEPARTMENT: 'วิศวกรรมผลิต 1',
   APP_NAME: 'Leave Management',
   MOBILE_ONLY: true,
